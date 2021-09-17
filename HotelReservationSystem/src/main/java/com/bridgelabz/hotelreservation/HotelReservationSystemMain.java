79cf97c5bc47479ba058e87e0e19b11d0d18f781
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-17 19:38:52 +0530

    ADDED: project is initialised

---
+++ HotelReservationSystem/src/main/java/com/bridgelabz/hotelreservation/HotelReservationSystemMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.hotelreservation;
+
+public class HotelReservationSystemMain {
+
+	public static void main(String[] args) {
+		System.out.println("******Welcome to Hotel Reservation System******");
+	}
+
+}
