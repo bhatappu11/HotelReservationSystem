520c0f590e9617e13cb64874afcd47110c0ecd9f
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-17 20:30:48 +0530

    ADDED: testcase to test addHotel

--- HotelReservationSystem/src/test/java/com/bridgelabz/hotelreservation/HotelReservationSystemTest.java
+++ HotelReservationSystem/src/test/java/com/bridgelabz/hotelreservation/HotelReservationSystemTest.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.hotelreservation;
 
-public class HotelReservationSystemTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class HotelReservationSystemTest {
+	
+	@Test
+	public void givenHotelDetails_WhenProper_ShouldReturnTrue() {
+		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
+		boolean result = hotel.addHotel("Lakewood", 100.0, 110.0);
+		Assert.assertTrue(result);
+		
+	}
 }
