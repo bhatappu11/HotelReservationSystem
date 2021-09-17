2a8716a498dc775746934b1890b58dc03899e937
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-17 22:53:04 +0530

    REAFCTOR: test case to get cheapest hotel based on weekdays and weekends

--- HotelReservationSystem/src/test/java/com/bridgelabz/hotelreservation/HotelReservationSystemTest.java
+++ HotelReservationSystem/src/test/java/com/bridgelabz/hotelreservation/HotelReservationSystemTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.hotelreservation;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,10 +19,10 @@ public class HotelReservationSystemTest {
 	@Test
 	public void givenDateRange_find_CheapestHotel() {
 		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",100.0,110.0);
-		hotel.addHotel("Bridgewood",110.0,110.0);
-		hotel.addHotel("Ridgewood",105.0,110.0);
-		Hotel cheapestHotel = hotel.getCheapestHotel("10Sep2021", "11Sep2021");
-		System.out.println(cheapestHotel.getHotelName());
+		hotel.addHotel("Lakewood",110.0,90.0);
+		hotel.addHotel("Bridgewood",150.0,50.0);
+		hotel.addHotel("Ridgewood",220.0,150.0);
+		List<Hotel> cheapestHotels = hotel.getCheapestHotel("10Sep2021", "11Sep2021");
+		System.out.println(cheapestHotels);
 	}
 }
