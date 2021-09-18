48b956918bc98e317d34d33fcbb7de9c4a50e3a7
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-18 07:32:29 +0530

    REFACTOR: used before to initialise the hotels and test case to check add hotel

--- HotelReservationSystem/src/test/java/com/bridgelabz/hotelreservation/HotelReservationSystemTest.java
+++ HotelReservationSystem/src/test/java/com/bridgelabz/hotelreservation/HotelReservationSystemTest.java
@@ -4,10 +4,26 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class HotelReservationSystemTest {
+	HotelReservationServiceIF hotel; 
+	@Before
+	public void initialize() {
+		hotel = new HotelReservationServiceImpl();
+		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
+		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
+		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
+	}
+	@Test
+	public void givenHotelData_whenAdded_ShouldGetTrue() {
+		HotelReservationServiceImpl hotel = new HotelReservationServiceImpl();
+		int oldSize = hotel.hotelList.size();
+		hotel.addHotel("Taj", 110.0, 90.0, 80.0, 70.0, 3);
+		Assert.assertSame(oldSize+1,hotel.hotelList.size());
+	}
 	
 	@Test
 	public void givenHotelDetails_WhenProper_ShouldReturnTrue() {
@@ -20,10 +36,6 @@ public class HotelReservationSystemTest {
 	
 	@Test
 	public void givenDateRange_find_CheapestHotel() {
-		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
-		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
-		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
 		List<Hotel> cheapestHotels = hotel.getCheapestHotel("10Sep2021", "11Sep2021",CustomerType.REGULAR);
 		System.out.println("cheapest hotels: "+cheapestHotels);
 		Assert.assertEquals("Lakewood", cheapestHotels.get(0).getHotelName());
@@ -32,35 +44,23 @@ public class HotelReservationSystemTest {
 	
 	@Test
 	public void givenDateRange_find_CheapestBestRatedHotel() {
-		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
-		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
-		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
 		Hotel cheapestBestRatedHotel = hotel.getCheapestBestRatedHotel("10Sep2021", "11Sep2021",CustomerType.REGULAR);
-		System.out.println("cheapest best rated: "+cheapestBestRatedHotel);
-		Assert.assertEquals("Bridgewood", cheapestBestRatedHotel.getHotelName());
+		System.out.println(" cheapest best rated: "+cheapestBestRatedHotel);
+		Assert.assertEquals(" Bridgewood", cheapestBestRatedHotel.getHotelName());
 		
 	}
 	
 	@Test
 	public void givenDateRange_find_BestRatedHotel() {
-		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
-		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
-		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
 		Hotel bestRatedHotel = hotel.getBestRatedHotel("10Sep2021", "11Sep2021",CustomerType.REGULAR);
-		System.out.println("Best rated: "+bestRatedHotel);
-		Assert.assertEquals("Ridgewood", bestRatedHotel.getHotelName());
+		System.out.println(" Best rated: "+bestRatedHotel);
+		Assert.assertEquals(" Ridgewood", bestRatedHotel.getHotelName());
 	}
 	
 	@Test
 	public void givenDateRangeAndRewardedUser_find_BestRatedHotel() {
-		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
-		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
-		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
 		Hotel bestRatedHotel = hotel.getBestRatedHotel("10Sep2021", "11Sep2021",CustomerType.REWARD);
-		System.out.println("the best rated hotel is : ");
+		System.out.println(" best rated hotel is : ");
 		System.out.println(hotel);
 		System.out.println();
 		Assert.assertEquals("Ridgewood", bestRatedHotel.getHotelName());
@@ -68,12 +68,8 @@ public class HotelReservationSystemTest {
 	
 	@Test
 	public void givenDateRangeAndRewardedCustomer_find_CheapestAndBestRatedHotel() {
-		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
-		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
-		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
 		Hotel bestRatedHotel = hotel.getBestRatedHotel("10Sep2021", "11Sep2021",CustomerType.REWARD);
-		System.out.println("the cheapest and best rated hotel is : ");
+		System.out.println("cheapest best rated hotel is : ");
 		System.out.println(bestRatedHotel);
 		System.out.println();
 		Assert.assertEquals("Ridgewood", bestRatedHotel.getHotelName());
@@ -82,23 +78,15 @@ public class HotelReservationSystemTest {
 	@Test
 	public void givenDateRangeAndRewardedCustomer_find_CheapestHotel() {
 		
-		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
-		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
-		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
 		Hotel bestRatedHotel = hotel.getBestRatedHotel("10Sep2021", "11Sep2021",CustomerType.REWARD);
-		System.out.println("the cheapest hotel is : ");
+		System.out.println(" cheapest hotel is : ");
 		System.out.println(bestRatedHotel);
 		System.out.println();
-		Assert.assertEquals("Ridgewood", bestRatedHotel.getHotelName());
+		Assert.assertEquals(" Ridgewood", bestRatedHotel.getHotelName());
 	}
 	
 	@Test
 	public void givenDateRange_IfNull_ShouldThrowException() {
-		HotelReservationServiceIF hotel = new HotelReservationServiceImpl();
-		hotel.addHotel("Lakewood",110.0,90.0,80.0,80.0,3);
-		hotel.addHotel("Bridgewood",150.0,50.0,110.0,50.0,4);
-		hotel.addHotel("Ridgewood",220.0,150.0,100.0,40.0,5);
 		ExpectedException exceptionRule = ExpectedException.none();
 		exceptionRule.expect(HotelManagementException.class);
 		try {
